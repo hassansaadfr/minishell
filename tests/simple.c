ce58d0b6ad21ff39e76d547086c5f1761fe31e41
hassansaadfr <saadaoui.hassan@laposte.net>
2021-04-05 16:00:18 +0200

    norme

--- tests/simple.c
+++ tests/simple.c
@@ -6,7 +6,7 @@
 /*   By: hsaadaou <hsaadaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 14:51:00 by hsaadaou          #+#    #+#             */
-/*   Updated: 2021/04/05 15:59:59 by hsaadaou         ###   ########.fr       */
+/*   Updated: 2021/04/05 16:00:13 by hsaadaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static int		ft_addition(int a, int b)
 }
 
 Test(misc, failing) {
-    cr_assert(ft_addition(1, 1) == 2);
+    cr_assert(ft_addition(1,      1) == 2);
 }
 
 Test(misc, passing) {
