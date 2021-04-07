8b8a569a3a34071c4b1aaf0c0b8dda07f9addabe
axaidan <axaidan@student.42.fr>
2021-04-07 17:41:36 +0200

    PROMPT -  prompt to displayed in stderr, env() working juste for initial char **envp

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hsaadaou <hsaadaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 12:43:29 by hsaadaou          #+#    #+#             */
-/*   Updated: 2021/04/07 15:07:02 by user42           ###   ########.fr       */
+/*   Updated: 2021/04/07 16:45:52 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,14 @@
 
 void	prompt(void)
 {
-	ft_putstr_fd("minishell> ", 1);
+	if (isatty(0))
+		ft_putstr_fd("minishell> ", 2);
+}
+
+void	env(char **envp)
+{
+	while (*envp)
+		printf("%s\n", *envp++);
 }
 
 int		main(int argc, char **argv, char **envp)
@@ -24,17 +31,14 @@ int		main(int argc, char **argv, char **envp)
 
 	(void)argc;
 	(void)argv;
-	(void)envp;
 	ret_gnl = 1;
 	line = NULL;
 	while (ret_gnl > 0)
 	{
+		prompt();
 		ret_gnl = get_next_line(0, &line);
-		if (*line)
-		{
-			prompt();
-			printf("%s\n", line);
-		}
+		if (ft_strncmp(line, "env", 4) == 0)
+			env(envp);
 		free(line);
 	}
 	return (1);
