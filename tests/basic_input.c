6ab7337f82a8c2a2e57169aa8b5cb322929cf630
hassansaadfr <saadaoui.hassan@laposte.net>
2021-04-09 14:58:12 +0200

    cd

--- tests/basic_input.c
+++ tests/basic_input.c
@@ -5,42 +5,62 @@
 Test(basic_input, basic_echo) {
 	int		fd;
 	char	*path;
-	char	buff[100];
 	int		diff;
 	char	*expected;
+	char	*file;
 
-	expected = "lol\n";
-	path = "tests/outputs_m_sh/basic_input.log";
-	system("echo 'echo -n lol' | ./minishell > tests/outputs_m_sh/basic_input.log");
+	expected = "lol";
+	path = "tests/outputs_m_sh/basic_echo.log";
+	system("echo -n 'echo -n lol' | ./minishell > tests/outputs_m_sh/basic_echo.log");
 	fd = open(path, O_RDONLY);
-	read(fd, buff, 100);
-	diff = ft_strncmp(expected, buff, 100);
+	get_next_line(fd, &file);
+	diff = ft_strncmp(expected, file, 100);
 	close(fd);
 	if (diff == 0)
-		system("rm -f tests/outputs_m_sh/basic_input.log");
+		system("rm -f tests/outputs_m_sh/basic_echo.log");
 	else
-		printf("+++++\nYour return: %s\nExpected: %s\n+++++\n", buff, expected);
+		printf("+++++\nYour return: %s\nExpected: %s\n+++++\n", file, expected);
 	cr_assert(diff == 0);
 }
 
 Test(basic_input, empty) {
 	int		fd;
 	char	*path;
-	char	buff[100];
 	int		diff;
 	char	*expected;
+	char	*file;
 
 	expected = "";
-	path = "tests/outputs_m_sh/basic_input.log";
-	system("echo 'echo -n' | ./minishell > tests/outputs_m_sh/basic_input.log");
+	path = "tests/outputs_m_sh/empty.log";
+	system("echo -n 'echo -n ' | ./minishell > tests/outputs_m_sh/empty.log");
 	fd = open(path, O_RDONLY);
-	read(fd, buff, 100);
-	diff = ft_strncmp(expected, buff, 100);
+	get_next_line(fd, &file);
+	diff = ft_strncmp(expected, file, 100);
 	close(fd);
 	if (diff == 0)
-		system("rm -f tests/outputs_m_sh/basic_input.log");
+		system("rm -f tests/outputs_m_sh/empty.log");
 	else
-		printf("+++++\nYour return: %s\nExpected: %s\n+++++\n", buff, expected);
+		printf("+++++\nYour return: %s\nExpected: %s\n+++++\n", file, expected);
 	cr_assert(diff == 0);
 }
 
+Test(basic_input, very_long_input) {
+	int		fd;
+	char	*path;
+	int		diff;
+	char	*expected;
+	char	*file;
+
+	expected = "aertyuioopqsdfghjkllmwxcvbn123456789&éaertyuioopqsdfghjkllmwxcvbn123456789&é";
+	path = "tests/outputs_m_sh/very_long_input.log";
+	system("echo -n 'echo -n aertyuioopqsdfghjkllmwxcvbn123456789&éaertyuioopqsdfghjkllmwxcvbn123456789&é' | ./minishell > tests/outputs_m_sh/very_long_input.log");
+	fd = open(path, O_RDONLY);
+	get_next_line(fd, &file);
+	diff = ft_strncmp(expected, file, 100);
+	close(fd);
+	if (diff == 0)
+		system("rm -f tests/outputs_m_sh/very_long_input.log");
+	else
+		printf("+++++\nYour return: %s\nExpected: %s\n+++++\n", file, expected);
+	cr_assert(diff == 0);
+}
--- tests/cd_test.c
+++ tests/cd_test.c
@@ -2,20 +2,135 @@
 #include "minishell.h"
 #include <fcntl.h>
 
-Test(cd_basic, passing) {
+Test(cd, directory_dont_exist) {
 	int		fd;
 	char	*path;
-	char	buff[100];
 	int		diff;
+	char	*expected;
+	char	*file;
 
-	path = "tests/outputs_m_sh/basic_input.log";
-	system("echo 'echo lol' | ./minishell  > tests/outputs_m_sh/basic_input.log");
+	expected = "minishell: No such file or directory";
+	path = "tests/outputs_m_sh/directory_dont_exist.log";
+	system("echo -n 'cd toto ' | ./minishell > tests/outputs_m_sh/directory_dont_exist.log");
 	fd = open(path, O_RDONLY);
-	read(fd, buff, 100);
-	diff = ft_strncmp("lol\n", buff, 50);
-	// cr_assert(diff == 0);
-	cr_assert(1);
+	get_next_line(fd, &file);
+	diff = ft_strncmp(expected, file, 100);
 	close(fd);
 	if (diff == 0)
-		system("rm tests/outputs_m_sh/basic_input.log");
+		system("rm -f tests/outputs_m_sh/directory_dont_exist.log");
+	else
+		printf("+++++\nYour return: %s\nExpected: %s\n+++++\n", file, expected);
+	cr_assert(diff == 0);
+}
+
+Test(cd, root_dir) {
+	int		fd;
+	char	*path;
+	int		diff;
+	char	*expected;
+	char	*file;
+
+	expected = "";
+	path = "tests/outputs_m_sh/root_dir.log";
+	system("echo -n 'cd / ' | ./minishell > tests/outputs_m_sh/root_dir.log");
+	fd = open(path, O_RDONLY);
+	get_next_line(fd, &file);
+	diff = ft_strncmp(expected, file, 100);
+	close(fd);
+	if (diff == 0)
+		system("rm -f tests/outputs_m_sh/root_dir.log");
+	else
+		printf("+++++\nYour return: %s\nExpected: %s\n+++++\n", file, expected);
+	cr_assert(diff == 0);
+}
+
+Test(cd, parent_dir) {
+	int		fd;
+	char	*path;
+	int		diff;
+	char	*expected;
+	char	*file;
+
+	expected = "";
+	path = "tests/outputs_m_sh/parent_dir.log";
+	system("echo -n 'cd .. ' | ./minishell > tests/outputs_m_sh/parent_dir.log");
+	fd = open(path, O_RDONLY);
+	get_next_line(fd, &file);
+	diff = ft_strncmp(expected, file, 100);
+	close(fd);
+	if (diff == 0)
+		system("rm -f tests/outputs_m_sh/parent_dir.log");
+	else
+		printf("+++++\nYour return: %s\nExpected: %s\n+++++\n", file, expected);
+	cr_assert(diff == 0);
+}
+
+Test(cd, parent_tricky) {
+	int		fd;
+	char	*path;
+	int		diff;
+	char	*expected;
+	char	*file;
+
+	expected = "/";
+	path = "tests/outputs_m_sh/parent_tricky.log";
+	system("echo -n 'cd srcs/../.. ; pwd' | ./minishell > tests/outputs_m_sh/parent_tricky.log");
+	fd = open(path, O_RDONLY);
+	get_next_line(fd, &file);
+	diff = ft_strncmp(expected, file, 100);
+	close(fd);
+	if (diff == 0)
+		system("rm -f tests/outputs_m_sh/parent_tricky.log");
+	else
+	{
+		printf("test: parent_tricky\n");
+		printf("+++++\nYour return: %s\nExpected: %s\n+++++\n", file, expected);
+	}
+	cr_assert(diff == 0);
+}
+
+Test(cd, the_root_parent) {
+	int		fd;
+	char	*path;
+	int		diff;
+	char	*expected;
+	char	*file;
+
+	expected = "/";
+	path = "tests/outputs_m_sh/the_root_parent.log";
+	system("echo -n 'cd /../.. ; pwd' | ./minishell > tests/outputs_m_sh/the_root_parent.log");
+	fd = open(path, O_RDONLY);
+	get_next_line(fd, &file);
+	diff = ft_strncmp(expected, file, 100);
+	close(fd);
+	if (diff == 0)
+		system("rm -f tests/outputs_m_sh/the_root_parent.log");
+	else
+	{
+		printf("test: the_root_parent\n");
+		printf("+++++\nYour return: %s\nExpected: %s\n+++++\n", file, expected);
+	}
+	cr_assert(diff == 0);
+}
+
+Test(cd, acces_denied) {
+	int		fd;
+	char	*path;
+	int		diff;
+	char	*expected;
+	char	*file;
+
+	expected = "minishell: Permission denied";
+	path = "tests/outputs_m_sh/acces_denied.log";
+	system("mkdir /toto; chmod 000 /toto");
+	system("echo -n 'cd /toto' | sudo -u user1 ./minishell > tests/outputs_m_sh/acces_denied.log");
+	fd = open(path, O_RDONLY);
+	get_next_line(fd, &file);
+	diff = ft_strncmp(expected, file, 100);
+	close(fd);
+	if (diff == 0)
+		system("rm -f tests/outputs_m_sh/acces_denied.log");
+	else
+		printf("+++++\nYour return: %s\nExpected: %s\n+++++\n", file, expected);
+	cr_assert(diff == 0);
 }
