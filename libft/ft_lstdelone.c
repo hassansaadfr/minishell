82e7d98cedd872e6ce885efbdd7ea1f9839f8ebb
hassansaadfr <saadaoui.hassan@laposte.net>
2021-04-10 15:07:26 +0200

    protect function lstdelone

--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstdelone.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: axaidan <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: hsaadaou <hsaadaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/25 19:08:28 by axaidan           #+#    #+#             */
-/*   Updated: 2020/09/25 19:08:55 by axaidan          ###   ########.fr       */
+/*   Updated: 2021/04/10 15:05:18 by hsaadaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@ void	ft_lstdelone(t_list *elem, void (*del)(void *))
 {
 	if (del == NULL || elem == NULL)
 		return ;
+	if (elem->previous)
+		elem->previous->next = elem->next;
+	if (elem->next)
+		elem->next->previous = elem->previous;
 	(*del)(elem->content);
 	free(elem);
 }
