9caa922dac80cefdfd1e23935c1fe6035bbd7783
hassansaadfr <saadaoui.hassan@laposte.net>
2021-04-12 21:46:21 +0200

    unset

--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -1,9 +1,30 @@
 #include "minishell.h"
 
+int			ft_unset_error(char **argv)
+{
+	int		size;
+
+	size = 0;
+	size = get_strarr_size(argv);
+	if (size == 1)
+	{
+		ft_putstr_fd("unset: not enough arguments\n", 2);
+		return (0);
+	}
+	return (1);
+}
+
 int			builtin_unset(char **argv, t_list *env_list)
 {
-	(void)argv;
-	(void)env_list;
-	printf("TODO unset\n");
-	return (1);
+	int		out;
+	int		i;
+
+	out = ft_unset_error(argv);
+	if (out)
+	{
+		i = 1;
+		while (argv[i])
+			delete_env(env_list, argv[i++]);
+	}
+	return (out);
 }
