c1b58796a1a59889f997a763ea32f20fcf31b5ec
hassansaadfr <saadaoui.hassan@laposte.net>
2021-04-13 17:36:22 +0200

    prompt

--- srcs/prompt.c
+++ srcs/prompt.c
@@ -7,9 +7,8 @@ void	prompt(t_list *env_list)
 
 	env_val = NULL;
 	tmp = get_env(env_list, "PWD");
-	if (!tmp)
-		return ;
-	env_val = ft_strchr(tmp->content, '=') + 1;
+	if (tmp)
+		env_val = ft_strchr(tmp->content, '=') + 1;
 	if (isatty(0))
 	{
 		ft_putstr_fd(env_val, 2);
