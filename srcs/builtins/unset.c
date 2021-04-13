d325f2a71b36d6be0d789051755e28d32a5c5544
hassansaadfr <saadaoui.hassan@laposte.net>
2021-04-13 16:37:42 +0200

    remove not enought argument

--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -1,19 +1,5 @@
 #include "minishell.h"
 
-int			ft_unset_error(char **argv)
-{
-	int		size;
-
-	size = 0;
-	size = get_strarr_size(argv);
-	if (size == 1)
-	{
-		ft_putstr_fd("unset: not enough arguments\n", 2);
-		return (0);
-	}
-	return (1);
-}
-
 int			unset(char *argv, t_list *env_list)
 {
 	return (delete_env(env_list, argv));
@@ -24,12 +10,8 @@ int			builtin_unset(char **argv, t_list *env_list)
 	int		out;
 	int		i;
 
-	out = ft_unset_error(argv);
-	if (out)
-	{
-		i = 1;
-		while (argv[i])
-			unset(argv[i++], env_list);
-	}
+	i = 1;
+	while (argv[i])
+		unset(argv[i++], env_list);
 	return (out);
 }
