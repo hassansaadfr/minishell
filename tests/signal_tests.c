82fdabef6e83c9d3eba69caa9704aa16031a1b00
axaidan <axaidan@student.42.fr>
2021-04-16 13:29:00 +0200

    encore un test dans tests/signal.c

--- tests/signal_tests.c
+++ tests/signal_tests.c
@@ -3,7 +3,7 @@
 #include "minishell.h"
 #include <sys/types.h>
 
-Test(signal_suite, toto, .signal = SIGINT)
+Test(signal_suite, sigint_basic, .signal = SIGINT)
 {
 	char	buff[100];
 	errno = 0;
@@ -21,3 +21,23 @@ Test(signal_suite, toto, .signal = SIGINT)
 	}
 	cr_expect(1);
 }
+
+Test(signal_suite, sigint_while_fork, .signal = SIGINT)
+{
+    char    buff[100];
+    errno = 0;
+    system("echo \"cat\" | ./minishell &");
+    system("pidof minishell > pidfile");
+    int fd = open("pidfile", O_RDONLY);
+    int readed = read(fd, buff, 100);
+    buff[readed] = 0;
+    kill(ft_atoi(buff), SIGINT);
+    struct stat sts;
+    close(fd);
+    system("rm pidfile");
+    if (stat(ft_strjoin("/proc/", buff), &sts) == -1 && errno == ENOENT) {
+        cr_expect(0);
+    }
+    cr_expect(1);
+}
+
