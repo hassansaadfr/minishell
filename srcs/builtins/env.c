b765a8ca7fa0808b7526ccf50ea342e84f450f4d
hassansaadfr <saadaoui.hassan@laposte.net>
2021-04-21 14:06:01 +0200

    env and export corrected

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -7,9 +7,12 @@ static void	print_env_list(t_list *env_list)
 	tmp = env_list;
 	while (tmp)
 	{
-		ft_putstr_fd((((t_env*)tmp->content)->name), STDOUT_FILENO);
-		ft_putstr_fd("=", STDOUT_FILENO);
-		ft_putendl_fd((((t_env*)tmp->content)->value), STDOUT_FILENO);
+		if (((t_env*)tmp->content)->value != NULL)
+		{
+			ft_putstr_fd((((t_env*)tmp->content)->name), STDOUT_FILENO);
+			ft_putstr_fd("=", STDOUT_FILENO);
+			ft_putendl_fd((((t_env*)tmp->content)->value), STDOUT_FILENO);
+		}
 		tmp = tmp->next;
 	}
 }
--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -1,5 +1,6 @@
 #include "minishell.h"
 
+
 static void	print_env_list(t_list *env_list)
 {
 	t_list	*tmp;
@@ -7,23 +8,17 @@ static void	print_env_list(t_list *env_list)
 	tmp = env_list;
 	while (tmp)
 	{
+		ft_putstr_fd("export ", STDOUT_FILENO);
 		ft_putstr_fd((((t_env*)tmp->content)->name), STDOUT_FILENO);
-		ft_putstr_fd("=\"", STDOUT_FILENO);
-		ft_putstr_fd((((t_env*)tmp->content)->value), STDOUT_FILENO);
-		ft_putstr_fd("\"\n", STDOUT_FILENO);
+		if (((t_env*)tmp->content)->value != NULL)
+		{
+			ft_putstr_fd("=\"", STDOUT_FILENO);
+			ft_putstr_fd((((t_env*)tmp->content)->value), STDOUT_FILENO);
+			ft_putstr_fd("\"", STDOUT_FILENO);
+		}
+		ft_putstr_fd("\n", STDOUT_FILENO);
 		tmp = tmp->next;
 	}
-}
-
-static char	*standardize_env(char *env)
-{
-	char	*str;
-
-	if (ft_strchr(env, '=') == NULL)
-		str = ft_strjoin(env, "=");
-	else
-		str = ft_strdup(env);
-	return (str);
 }
 
 int			builtin_export(char **argv, t_list *env_list)
@@ -42,7 +37,7 @@ int			builtin_export(char **argv, t_list *env_list)
 	{
 		while (argv[i])
 		{
-			env = standardize_env(argv[i]);
+			env = ft_strdup(argv[i]);
 			done = new_env(env_list, env);
 			if (!done)
 				done = edit_env(env_list, env);
