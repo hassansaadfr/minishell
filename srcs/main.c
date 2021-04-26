7223df00d855741e082655ed1c425b712c6edabd
axaidan <axaidan@student.42.fr>
2021-04-26 15:20:33 +0200

    MAIN - refactored minishell() for tty use and non-tty use

--- srcs/main.c
+++ srcs/main.c
@@ -6,49 +6,70 @@ void	signal_handling_register(void)
 	signal(SIGQUIT, SIG_IGN);
 }
 
-int		minishell(t_termios orig_termios)
+/*
+** TODO:
+** SHOULD REMOVE orig_termios FROM exec() FUNCTIONS
+** THEN SHOULD ACTIVATE NON-CANONICAL MODE BEFORE write_buffer()
+** AND THEN SHOULD DEACTIVATE NON-CANONICAL MODE AFTER
+*/
+
+static int	minishell_tty(t_termios orig_termios)
 {
-	int		stop;
-	t_list	*history;
-	t_buff	buff;
-	int		ret_gnl;
-	char	*line;
+	int     stop;
+	t_list  *history;
+	t_buff  buff;
 
 	signal_handling_register();
 	init_buff_and_history(&buff, &history);
 	stop = 0;
+	while (stop == 0)
+	{
+		prompt();
+		stop = write_buffer(&stop, &buff, history);
+		if (stop == 0)
+		{
+			stop = add_to_history(&buff, &history);
+			if (stop == 0)
+				exec(parse(buff.buffer), global.env_list, orig_termios);
+		}
+	}
+    ft_lstclear(&history, free);
+    free(buff.buffer);
+    free(buff.backup);
+
+	return (stop);
+}
+
+static int	minishell_non_tty(t_termios orig_termios)
+{
+	int     ret_gnl;
+	char    *line;
+	char	***cmds;
+
 	line = NULL;
 	ret_gnl = 1;
+    signal_handling_register();
+	while (ret_gnl > 0)
+	{
+		ret_gnl = get_next_line(STDIN_FILENO, &line);
+		if (*line)
+		{
+			cmds = parse(line);
+			exec(cmds, global.env_list, orig_termios);
+		}
+		free(line);
+	}
+	return (0);
+}
+
+int		minishell(t_termios orig_termios)
+{
+	int		ret;
+
 	if (isatty(STDIN_FILENO))
-	{
-		while (stop == 0)
-		{
-			// ENABLE RAW_MODE ICI
-			prompt();
-			stop = write_buffer(&stop, &buff, history);
-			// DISBLE RAW_MODE ICI
-			if (stop == 0)
-			{
-				stop = add_to_history(&buff, &history);
-				if (stop == 0)
-					exec(parse(buff.buffer), global.env_list, orig_termios);
-			}
-		}
-	}
+		ret = minishell_tty(orig_termios);
 	else
-	{
-		while (ret_gnl > 0)
-		{
-			prompt();
-			ret_gnl = get_next_line(0, &line);
-			if (*line)
-				exec(parse(line), global.env_list, orig_termios);
-			free(line);
-		}
-	}
-	ft_lstclear(&history, free);
-	free(buff.buffer);
-	free(buff.backup);
+		ret = minishell_non_tty(orig_termios);
 	return (0);
 }
 
