53fa97ca4d496ac65e8f5011d27452fc7af8f46b
hassansaadfr <saadaoui.hassan@laposte.net>
2021-04-29 16:23:17 +0200

    echo

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -3,8 +3,28 @@
 
 int			builtin_echo(char **argv, t_list *env_list)
 {
-	(void)argv;
+	int		i;
+	int		nl;
+	int		nb_arg;
+
+	i = 0;
+	nl = 0;
 	(void)env_list;
-	printf("TODO echo\n");
-	return (1);
+	argv++;
+	if (*argv && ft_strncmp(argv[0], "-n", 3) == 0)
+	{
+		nl = 1;
+		argv++;
+	}
+	nb_arg = get_strarr_size(argv);
+	while (argv[i])
+	{
+		ft_putstr_fd(argv[i], STDOUT_FILENO);
+		if (i < nb_arg - 1)
+			ft_putchar_fd(' ', STDOUT_FILENO);
+		i++;
+	}
+	if (!nl)
+		ft_putchar_fd('\n', STDOUT_FILENO);
+	return (0);
 }
