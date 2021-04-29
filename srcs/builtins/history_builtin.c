feb53adb2f8d6be2143ccc62bd5d09e2fb520a60
hassansaadfr <saadaoui.hassan@laposte.net>
2021-04-29 20:53:20 +0200

    history

---
+++ srcs/builtins/history_builtin.c
@@ -0,0 +1,31 @@
+#include "minishell.h"
+
+int		builtin_history(char **argv, t_list *env_list, t_list *history)
+{
+	int		i;
+	int		size;
+	t_list*	tmp;
+
+	(void)env_list;
+	size = 0;
+	i = 0;
+	tmp = ft_lstlast(history);
+	argv++;
+	size = get_strarr_size(argv);
+	if (size == 0)
+	{
+		while (tmp)
+		{
+			ft_putnbr_fd(i++, STDOUT_FILENO);
+			ft_putstr_fd('  ', STDOUT_FILENO);
+			ft_putendl_fd(tmp->content, STDOUT_FILENO);
+			tmp = tmp->previous;
+		}
+	}
+	else
+	{
+		ft_putendl_fd("minishell: history: Argument list too long", STDOUT_FILENO);
+		return (0);
+	}
+	return (1);
+}
