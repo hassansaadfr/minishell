dd0cec44bd67f605f2b4e2e6064ea7484c8ead4e
hassansaadfr <saadaoui.hassan@laposte.net>
2021-04-30 17:18:30 +0200

    leaks detected with this main

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+int	main(void)
+{
+	char	*str;
+
+	str = ft_alloc(sizeof(char) * 3);
+	str[0] = '1';
+	str[1] = '2';
+	str[2] = 0;
+	ft_putendl_fd(str, STDOUT_FILENO);
+	ft_exit_free();
+	ft_putendl_fd(str, STDOUT_FILENO);
+	return (0);
+}
