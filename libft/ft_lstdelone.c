f889bf1408454f9c362a8a37f681c98468063ad2
hassansaadfr <saadaoui.hassan@laposte.net>
2021-05-04 18:47:21 +0200

    delone-ok

--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -6,32 +6,40 @@
 /*   By: hsaadaou <hsaadaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/25 19:08:28 by axaidan           #+#    #+#             */
-/*   Updated: 2021/05/04 16:38:07 by hsaadaou         ###   ########.fr       */
+/*   Updated: 2021/05/04 18:40:51 by hsaadaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+** NULL-X-NULL		=> NULL				/// If NO prv and NO next ELEM = NULL
+** NULL-X-0-NULL	=> NULL-0-NULL		/// if NO prv and next ELEM = next;
+** NULL-0-X-NULL	=> NULL-0-NULL		/// if prv and NO next ELEM = prev;
+** NULL-0-X-0-NULL	=> NULL-0-0-NULL 	/// if prev and next ELEM = prev;
+*/
+
 void	ft_lstdelone(t_list **elem, void (*del)(void *))
 {
 	t_list	*tmp;
 
-	tmp = NULL;
-	if (del == NULL || elem == NULL)
+	if (del == NULL || *elem == NULL)
 		return ;
-	if ((*elem)->previous)
-		(*elem)->previous->next = (*elem)->next;
-	else
-		tmp = (*elem)->next;
-	if ((*elem)->next)
-		(*elem)->next->previous = (*elem)->previous;
-	(*del)((*elem)->content);
-	free(*elem);
-	// if (tmp)
-		*elem = tmp;
+	tmp = *elem;
+	if ((*elem)->previous == NULL && (*elem)->next == NULL)
+		*elem = NULL;
+	else if ((*elem)->previous == NULL && (*elem)->next != NULL)
+		*elem = (*elem)->next;
+	else if ((*elem)->previous != NULL && (*elem)->next == NULL)
+	{
+		*elem = (*elem)->previous;
+		(*elem)->next = NULL;
+	}
+	else if ((*elem)->previous != NULL && (*elem)->next != NULL)
+	{
+		tmp->previous->next = tmp->next;
+		tmp->next->previous = tmp->previous;
+	}
+	del(tmp->content);
+	free(tmp);
 }
-
-// NULL-X-NULL			=> NULL
-// NULL-X-0-NULL		=> NULL-0-NULL
-// NULL-0-X-NULL		=> NULL-0-NULL		/// SIZE ?
-// NULL-0-X-0-NULL		=> NULL-0-0-NULL 	/// OK
---
+++ tests/ft_lstdelone_main.c
@@ -0,0 +1,58 @@
+#include "libft.h"
+#include <stdio.h>
+
+void	display_lst(t_list *hist)
+{
+	int		i;
+
+	i = 0;
+	while (hist)
+	{
+        printf("%d\tcur: %p\t\"%10.10s\"\tprv: %15p\tnxt: %15p\n", i++, hist,
+            (char*)hist->content, hist->previous, hist->next);
+		hist = hist->next;
+	}
+	printf("fini\n");
+}
+
+
+void	create_list(t_list **lst)
+{
+	ft_lstadd_back(lst, ft_lstnew(ft_strdup("1")));
+	ft_lstadd_back(lst, ft_lstnew(ft_strdup("2")));
+	ft_lstadd_back(lst, ft_lstnew(ft_strdup("3")));
+	ft_lstadd_back(lst, ft_lstnew(ft_strdup("4")));
+
+}
+
+void	testlst()
+{
+	t_list	*lst;
+
+	lst = NULL;
+	create_list(&lst);
+	display_lst(lst);
+	ft_lstdelone(&lst->next, free);
+	ft_putstr_fd("+++++++\n", 0);
+	display_lst(lst);
+	ft_lstclear(&lst, free);
+}
+
+
+
+int	main(void)
+{
+	// char	*str;
+
+	// str = ft_alloc(sizeof(char) * 3);
+	// str[0] = '1';
+	// str[1] = '2';
+	// str[2] = 0;
+	// ft_putendl_fd(str, STDOUT_FILENO);
+	// // ft_free_ptr((void**)(&str));
+	// ft_exit_free();
+	// // str = NULL;
+	// ft_putendl_fd(str, STDOUT_FILENO);
+	testlst();
+	return (0);
+}
