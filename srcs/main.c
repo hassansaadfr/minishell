fc22bb8dfe1814716f9010bb801621ba43ec51de
axaidan <axaidan@student.42.fr>
2021-05-11 14:54:11 +0200

    MERGE - replugged exec() with main.c as in master

--- srcs/main.c
+++ srcs/main.c
@@ -7,57 +7,41 @@ void		signal_handling_register(void)
 }
 
 /*
-** TODO:
-** SHOULD REMOVE orig_termios FROM exec() FUNCTIONS
-** THEN SHOULD ACTIVATE NON-CANONICAL MODE BEFORE write_buffer()
-** AND THEN SHOULD DEACTIVATE NON-CANONICAL MODE AFTER
-*/
+ ** TODO:
+ ** SHOULD REMOVE orig_termios FROM exec() FUNCTIONS
+ ** THEN SHOULD ACTIVATE NON-CANONICAL MODE BEFORE write_buffer()
+ ** AND THEN SHOULD DEACTIVATE NON-CANONICAL MODE AFTER
+ */
 
-static int	minishell_tty(void)
+static int	minishell_tty(t_termios orig_termios)
 {
 	int		stop;
 	t_list	*history;
 	t_input	buff;
-	t_list	*tokens;
 
 	signal_handling_register();
 	init_buff_and_history(&buff, &history);
 	stop = 0;
-	tokens = NULL;
 	while (stop == 0)
 	{
 		prompt();
 		stop = write_buffer(&stop, &buff, history);
 		if (stop == 0)
 			stop = add_to_history(&buff, &history);
-		if (stop == 0 && not_empty(buff.buffer))
-			tokens = parsing(buff.buffer);
-		if (tokens && DEBUG)
-			display_tokens(tokens);
-		if (tokens)
-			ft_lstclear(&tokens, free_token);
+		if (stop == 0)
+			exec(parse(buff.buffer), g_global.env_list, orig_termios, history);
 	}
-	if (history)
-		ft_lstclear(&history, free);
-	if (buff.buffer)
-		free(buff.buffer);
-	if (buff.backup)
-		free(buff.backup);
+	ft_lstclear(&history, free);
+	free(buff.buffer);
+	free(buff.backup);
 	return (stop);
 }
-
-/*
-**	ANCIENT EXEC COMMAND : (in place of (void)cmds; (void)orig_termios;
-**	cmds = parse(line);
-**	exec(cmds, g_global.env_list, orig_termios);
-*/
 
 static int	minishell_non_tty(t_termios orig_termios)
 {
 	int		ret_gnl;
 	char	*line;
 	char	***cmds;
-	t_list	*tokens;
 
 	line = NULL;
 	ret_gnl = 1;
@@ -67,11 +51,8 @@ static int	minishell_non_tty(t_termios orig_termios)
 		ret_gnl = get_next_line(STDIN_FILENO, &line);
 		if (*line)
 		{
-			tokens = parsing(line);
-			display_tokens(tokens);
-			ft_lstclear(&tokens, free);
-			(void)orig_termios;
-			(void)cmds;
+			cmds = parse(line);
+			exec(cmds, g_global.env_list, orig_termios, NULL);
 		}
 		free(line);
 	}
@@ -81,7 +62,7 @@ static int	minishell_non_tty(t_termios orig_termios)
 int			minishell(t_termios orig_termios)
 {
 	if (isatty(STDIN_FILENO))
-		return (minishell_tty());
+		return (minishell_tty(orig_termios));
 	else
 		return (minishell_non_tty(orig_termios));
 }
