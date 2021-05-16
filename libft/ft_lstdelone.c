78b146b71af6ae2a0a0af98103d4371a2831e7e2
axaidan <axaidan@student.42.fr>
2021-05-16 16:35:55 +0200

    EXEC - splitting args and redirs working, modified ft_lstdelone and ft_lstaddback

--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -6,24 +6,54 @@
 /*   By: hsaadaou <hsaadaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/25 19:08:28 by axaidan           #+#    #+#             */
-/*   Updated: 2021/05/04 19:11:35 by hsaadaou         ###   ########.fr       */
+/*   Updated: 2021/05/16 16:29:12 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-** NULL-X-NULL		=> NULL				/// If NO prv and NO next ELEM = NULL
-** NULL-X-0-NULL	=> NULL-0-NULL		/// if NO prv and next ELEM = next;
-** NULL-0-X-NULL	=> NULL-0-NULL		/// if prv and NO next ELEM = prev;
-** NULL-0-X-0-NULL	=> NULL-0-0-NULL 	/// if prev and next ELEM = prev;
-*/
+ ** NULL-X-NULL		=> NULL				/// If NO prv and NO next ELEM = NULL
+ ** NULL-X-0-NULL	=> NULL-0-NULL		/// if NO prv and next ELEM = next;
+ ** NULL-0-X-NULL	=> NULL-0-NULL		/// if prv and NO next ELEM = prev;
+ ** NULL-0-X-0-NULL	=> NULL-0-0-NULL 	/// if prev and next ELEM = prev;
+ */
+
+/*
+   void	ft_lstdelone(t_list **elem, void (*del)(void *))
+   {
+   t_list	*tmp;
+
+   if (del == NULL || *elem == NULL)
+   return ;
+   tmp = *elem;
+   if ((*elem)->previous == NULL && (*elem)->next == NULL)
+ *elem = NULL;
+ else if ((*elem)->previous == NULL && (*elem)->next != NULL)
+ {
+ *elem = (*elem)->next;
+ (*elem)->previous = NULL;
+ }
+ else if ((*elem)->previous != NULL && (*elem)->next == NULL)
+ {
+ *elem = (*elem)->previous;
+ (*elem)->next = NULL;
+ }
+ else if ((*elem)->previous != NULL && (*elem)->next != NULL)
+ {
+ tmp->previous->next = tmp->next;
+ tmp->next->previous = tmp->previous;
+ }
+ del(tmp->content);
+ free(tmp);
+ }
+ */
 
 void	ft_lstdelone(t_list **elem, void (*del)(void *))
 {
 	t_list	*tmp;
 
-	if (del == NULL || *elem == NULL)
+	if (*elem == NULL)
 		return ;
 	tmp = *elem;
 	if ((*elem)->previous == NULL && (*elem)->next == NULL)
@@ -35,14 +65,18 @@ void	ft_lstdelone(t_list **elem, void (*del)(void *))
 	}
 	else if ((*elem)->previous != NULL && (*elem)->next == NULL)
 	{
-		*elem = (*elem)->previous;
-		(*elem)->next = NULL;
+//		*elem = (*elem)->previous;
+//		(*elem)->next = NULL;
+		(*elem)->previous->next = NULL;
 	}
 	else if ((*elem)->previous != NULL && (*elem)->next != NULL)
 	{
 		tmp->previous->next = tmp->next;
 		tmp->next->previous = tmp->previous;
 	}
-	del(tmp->content);
-	free(tmp);
+	if (del)
+	{
+		del(tmp->content);
+		free(tmp);
+	}
 }
--- srcs/exec2/exec2.c
+++ srcs/exec2/exec2.c
@@ -1,17 +1,17 @@
 #include "minishell.h"
 
-void	display_splitted_cmd(t_list *cmd, int debug_i)
+void	display_splitted_cmd(t_list *cmd, int debug_i, char *type)
 {
-	printf("========================== CMD %2i ==========================\n",
-			debug_i++);
+	printf("========================== %3s %2i ==========================\n",
+			type, debug_i++);
 	display_tokens(cmd);
 }
 
 void	del_smc_node(t_list *tmp)
 {
-		tmp->previous = NULL;
-		tmp->next = NULL;
-		ft_lstdelone(&tmp, free_token);
+	tmp->previous = NULL;
+	tmp->next = NULL;
+	ft_lstdelone(&tmp, free_token);
 }
 
 t_list	*split_smc(t_list **tokens)
@@ -52,16 +52,65 @@ t_list	*find_first_arg_node(t_list *tokens)
 	return (NULL);
 }
 
-int		execute_cmd(t_list *tokens, t_list *env_list)
+t_list	*isolate_redirs(t_list **redirs_and_args)
 {
-	int		ret_exec;
-	char	*path;
-	char	**argv;
-	t_list	*first_arg_node;
+	t_list	*redirs;
+	t_token	*token;
+	t_list	*tmp;
+	t_list	*to_remove;
 
-	first_arg_node = find_first_arg_node(tokens);
+	redirs = NULL;
+	// DEBUT DE LISTE
+	while (*redirs_and_args && 
+			(is_redir(((t_token*)(*redirs_and_args)->content)->type)
+			 || ((t_token*)(*redirs_and_args)->content)->type == FD))
+	{
+		tmp = *redirs_and_args;
+		*redirs_and_args = (*redirs_and_args)->next;
+		tmp->next = NULL;
+		tmp->previous = NULL;
+		ft_lstadd_back(&redirs, tmp);
+	}
+	// EN MILIEU DE LISTE
+	token = NULL;
+	tmp = *redirs_and_args;
+	while (tmp)
+	{
+		token = NULL;
+		tmp = *redirs_and_args;
+		while (tmp)
+		{
+			to_remove = tmp;
+			token = to_remove->content;
+			tmp = tmp->next;
+			if (is_redir(token->type) || token->type == FD)
+			{
+				ft_lstdelone(&to_remove, NULL);
+				to_remove->previous = NULL;
+				to_remove->next = NULL;
+				ft_lstadd_back(&redirs, to_remove);
+			}
+		}
+	}
+	return (redirs);
+}
+
+int		execute_simple_cmd(t_list *tokens, t_list *env_list, int debug_i)
+{
+	(void)env_list;
+	int		ret_exec;
+	//	char	**argv;
+	t_list	*redirs;
+
+	redirs = isolate_redirs(&tokens);
+	if (tokens)
+		display_splitted_cmd(tokens, debug_i, "CMD");
+	if (redirs)
+		display_splitted_cmd(redirs, debug_i, "RDR");
+	//	argv = make_argv_arr(tokens); 
 
 	ret_exec = 0;
+	return (0);
 }
 
 int		executing(t_list *tokens, t_list *env_list, t_list *history)
@@ -69,19 +118,19 @@ int		executing(t_list *tokens, t_list *env_list, t_list *history)
 	(void)env_list;
 	(void)history;
 	int		debug_i = 0;
-	t_list	*cmd;
+	t_list	*indpdt_cmd;
 	int		ret_exec;
 
-	cmd = NULL;
+	indpdt_cmd = NULL;
 	ret_exec = 0;
 	while (tokens)
 	{
-		cmd = split_smc(&tokens);
-		display_splitted_cmd(cmd, debug_i++);
-//		ret_exec = execute(cmd, env_list);
-		ft_lstclear(&cmd, free_token);
-		if (ret_exec != 0)
-			ft_lstclear(&tokens, free_token);
+		indpdt_cmd = split_smc(&tokens);
+		//	display_splitted_cmd(indpdt_cmd, debug_i++, "CMD");
+		// 1 - EXPANSION
+		// 2 - EXECUTION
+		ret_exec = execute_simple_cmd(indpdt_cmd, env_list, debug_i++);
+		ft_lstclear(&indpdt_cmd, free_token);
 	}
 	return (ret_exec);
 }
