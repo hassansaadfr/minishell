2b8d9aa29c0d60b5a00cf91182495fde4d1b012a
axaidan <57766473+axaidan@users.noreply.github.com>
2021-06-07 17:18:33 +0200

    Update errors.c

--- srcs/utils/errors.c
+++ srcs/utils/errors.c
@@ -4,7 +4,7 @@ void		print_err(char *binary, char *arg, char *err)
 {
 	ft_putstr_fd("minishell: ", STDERR_FILENO);
 	if (!isatty(STDIN_FILENO))
-		ft_putstr_fd(" line 1: ", STDERR_FILENO);
+		ft_putstr_fd("line 1: ", STDERR_FILENO);
 	if (binary)
 	{
 		ft_putstr_fd(binary, STDERR_FILENO);
