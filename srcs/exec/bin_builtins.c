16de6c97374a6a6ce3d5292fee07420daf14f69a
hassansaadfr <saadaoui.hassan@laposte.net>
2021-06-17 15:27:49 +0200

    done

--- srcs/exec/bin_builtins.c
+++ srcs/exec/bin_builtins.c
@@ -2,24 +2,27 @@
 
 int	exec_from_builtins(char **argv, t_list *env_list, t_list *history)
 {
+	int		out;
+
 	if (ft_strcmp(*argv, "history") == 0)
-		return (builtin_history(argv, env_list, history));
+		out = builtin_history(argv, env_list, history);
 	if (ft_strcmp(*argv, "pwd") == 0)
-		return (builtin_pwd(argv, env_list));
+		out = builtin_pwd(argv, env_list);
 	if (ft_strcmp(*argv, "echo") == 0)
-		return (builtin_echo(argv, env_list));
+		out = builtin_echo(argv, env_list);
 	if (ft_strcmp(*argv, "unset") == 0)
-		return (builtin_unset(argv, env_list));
+		out = builtin_unset(argv, env_list);
 	if (ft_strcmp(*argv, "export") == 0)
-		return (builtin_export(argv, env_list));
+		out = builtin_export(argv, env_list);
 	if (ft_strcmp(*argv, "exit") == 0)
-		return (builtin_exit(argv, env_list));
+		out = builtin_exit(argv, env_list);
 	if (ft_strcmp(*argv, "cd") == 0)
-		return (builtin_cd(argv, env_list));
+		out = builtin_cd(argv, env_list);
 	if (ft_strcmp(*argv, "env") == 0)
-		return (env(env_list));
-	edit_underscore_var(env_list, argv[get_strarr_size(argv) - 1]);
-	return (0);
+		out = env(env_list);
+	if (out == 0)
+		edit_underscore_var(env_list, argv[get_strarr_size(argv) - 1]);
+	return (out);
 }
 
 int	is_builtin(char **argv)
